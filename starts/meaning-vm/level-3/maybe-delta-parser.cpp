6b87398ee5d6a4effbb066de5d88065ccdf60c2c
olpc user <olpc@xo-5d-f7-86.localdomain>
2020-01-11 17:48:33 -0800

    randomcode draft

--- starts/meaning-vm/level-3/maybe-delta-parser.cpp
+++ starts/meaning-vm/level-3/maybe-delta-parser.cpp
@@ -9,32 +9,139 @@ using namespace std;
 
 namespace delta {
 
+// SYNTAX PROPOSAL DEVELOPMENT
+// list of words, equivalent to file
+// words are processes, take ordered parameters
+// ideas:
+// parameters may be expressions
+// 'ref' expression takes 1 symbol, looks up symbols in context  (words are symbols)
+// 'context' expression sets context
+// 'set context symbol value' sets symbol value in context
+// [ ] ?
+//
+// link a b c 
+// is same as
+// set a b c
+//
+// concepts are contexts. <= simplifies a lot.  WE DO NOT NEED TO SOLVE EVERYTHING.
+//
+// concepts are contexts; if a word is in the active context, it is looked up in it automatically?
+//
+// simlifying the code to a list of words is helpful.  given you have a parser, it may make sense to store code as
+// lists of words.  this maks it easier to generate.  atm code is stored by different kinds of graph structures.
+// 	<karl is trying to relate around value of graph strucure vs just words>
+// 	<graph structure provides for code-that-is-code-at-all>
+// 	<making a random list of words means we need to bootstrap enough relevence to actually do anything>
+// 	<we could do that, for example linking each word to possible relevent words>
+// 		graph structure sounds more efficient.  the structure is a litle overcomplex, but mostly karl is
+// 		inhibited from thinking around it. delta may have a quality idea that helps connect with their resources,
+// 		but the idea doesn't seem complete enough to be efficient to do.  -advisor
+//
+//
+// we have to decide when to treat a wordsymbol as a process to evaluate
+// karl proposes check context
+// so we need a process to make a process
+//
+// process [ ] maybe
+// propose [ ] is special and makes a list of words as an expression
+
 void parsefile(string filename)
 {
 	ifstream ss(filename);
 	// need language to do things in.  evaluate habits.
 	// is file.  lines of text, start to end.
 	// how about 'use <process>' lines make the whole chunk be treated by process.
+	// source wants array indices.  could require major restructure.
+	// we could simulate by walking links.  or using numbers as relative references.
+	// we like the learning-numbers-like-humans approach.  let's do that one.
 	ref fileref = makeconcept();
+	fileref.link("filename", filename);
+	ref lastline, nextline, lastword, nextword;
 	while (true) {
 		string line;
 		getline(ss, line);
 		if (!ss) break;
 
 		ref lineref = makeconcept();
+		if (lastline != "nothing") {
+			lineref.link("previous", lastline);
+			lastline.link("next", lineref);
+		}
+		lastline = lineref;
 
 		stringstream words(line);
 		while (true) {
 			string word;
 			words >> word;
-			lineref.link("word", word);
+			ref wordref = makeconcept();
+			lineref.link("word", wordref);
+			wordref.link("line", lineref);
+			if (lastword != "nothing") {
+				wordref.link("previous", lastword);
+				lastword.link("next", wordref);
+			}
+			lastword = wordref;
 		}
 		fileref.link("line", line);
+		line.link("file", fileref);
+	}
+
+	void parse(ref firstword)
+	{
+		if (firstword == "run") {
+			// find process and evaluate
+		}
+	}
+
+	void init(string filename)
+	{
+		ref file = parsefile(filename);
+		ref firstword = file.getAll("line").front().getAll("word").front();
+		
+		// the world is made of concepts, which are made of typed links to other
+		// concepts.  like apple color red
+		map<string,ref> processes = {
+			{"using",
+				// process says how to turn words into references when used
+			},
+			{"run".
+				// find next word and evaluate as process
+			},{"store",
+				// evaluate as store "name" until "terminator"
+				// maybe ??
+			},{"get",
+				// evaluate as get "name" "type", puts on stack
+				// maybe ??
+				// it sounds like you don't want any variables.
+				// you act only on the contents of the file.
+				// you want to produce new files, maybe?
+				// 	want to produce processes that produce processes
+				// 	act on streams of words to make more.
+				// okay, let's say output is appended to the stream.
+				// maybe we could have a focus pointer in the working stream
+				// build links between words based on focuses
+				// we might need a way to hold a reference to other words
+				// maybye overlapping streams, and the process decides how
+				// to move them ...
+				// how about each word can be a stream.  we start in file
+				// stream.
+				// it sounds like this approach is highly immature.  shall
+				// we go back to the existing approach, premade, yes.
+			},{"link",
+				// considering link "name" "type" "target"
+			}
+		};
+		// wordref word1 word2 word3
+		// lineref line1 line2 line3
+		// goto line1
+		// examine line2 put in line4 <- this doesn't make for turing machine
+		//  [it sounds like you want me to fill in a generic programming language.  i don't have a lot of capacity for that with the uh control going on.  takes a long time.  wrote one already, but doesn't match your preferences]
+		//
 	}
 }
 
 // delta processes for bootstrapping
-// [the lines _are_ the code.  no need or special line thing.  just self-reference it seems.]
+// [the lines _are_ the code.  no need for special line thing.  just self-reference it seems.]
 // okay, so we have active-process, and move to next-process.  each line could be a process,
 // or each word.  maybe each word is possibly a process, can check next-word, previous-word.
 // and then new words are made, nd resulting file could be output for next iteration.
