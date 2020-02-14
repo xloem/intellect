f7778fcae27f10037ec7690f98593fa5d9d23cb9
Karl Semich <karl@Karl-Semich--0xloem-at-gmail-dot-com--txt-1-207-518-8256.Caring.goals.words.and.actions.are.more.important.than.anything>
2020-02-14 08:39:55 -0500

    fixes for gcc 7.4

--- starts/meaning-vm/level-2/funcs.hpp
+++ starts/meaning-vm/level-2/funcs.hpp
@@ -30,7 +30,7 @@ ref getnamed(std::string name, bool create = false);
 ref makehabit(ref name, std::initializer_list<ref> infonames, std::function<void(ref)> code);
 ref makehabitinformationorder(ref habit);
 void habitassume(ref habit, ref information, ref value);
-ref dohabit(ref habit, std::initializer_list<ref> args = {});
+ref dohabit(ref habit, std::initializer_list<ref> args = std::initializer_list<ref>());
 ref dohabit(ref habit, std::initializer_list<std::initializer_list<ref>> args, bool extra_information = false);
 
 void rethrowref();
