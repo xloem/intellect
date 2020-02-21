010482a86bdc5a4fb9ce27c2c2e370395f2f695c
Karl Semich <karl@Karl-Semich--0xloem-at-gmail-dot-com--txt-1-207-518-8256.Caring.goals.words.and.actions.are.more.important.than.anything>
2020-02-21 09:02:10 -0500

    working on consideration bubbles in very very small steps, with lots of distraction

--- starts/meaning-vm/level-2/funcs.hpp
+++ starts/meaning-vm/level-2/funcs.hpp
@@ -20,6 +20,8 @@ void checknotepad(ref concept);
 bool innotepad(ref concept, ref pad);
 void leavenotepad(ref concept, ref pad);
 void considerwith(ref realitypad, ref imaginationpad);
+ref imagineget(ref pad, ref concept);
+ref imagineset(ref pad, ref concept);
 void entersubnotepad(ref concept, ref name, bool allowouter = false);
 //void givename(ref context, ref concept, std::string const & name, bool contextisnotepad = false);
 //ref namelookup(std::string name, ref context, ref outerlink, bool makelocal, bool contextisnotepad = false);
