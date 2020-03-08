bd5cf1d48bdc4468d812dd424880ead37ac86f0d
Karl Semich <karl@Karl-Semich--0xloem-at-gmail-dot-com--txt-1-207-518-8256.Caring.goals.words.and.actions.are.more.important.than.anything>
2020-03-08 04:01:41 -0400

    karl says relevancy is the most important thing to make a computer sentient

--- starts/push/find_relevant_string.cpp
+++ starts/push/find_relevant_string.cpp
@@ -2,6 +2,33 @@
 
 #include <cassert>
 
+#include <iostream>
+
+std::vector<std::string> string_split(std::string const & string, std::string const & delimiter)
+{
+	std::vector<std::string> substrings;
+	size_t lastidx = 0, idx;
+	while ((idx = string.find("-", lastidx)) != std::string::npos) {
+		substrings.push_back(string.substr(lastidx, idx - lastidx));
+		lastidx = idx + 1;
+	}
+	substrings.push_back(string.substr(lastidx));
+	return substrings;
+}
+
+bool already_tried(std::string const & context, std::string const & idea)
+{
+	auto & set = sets[context];
+	auto string = context + "-" + idea;
+	if (set.count(string) == 0) {
+		std::cerr << " consider '" << context << "' '" << idea << "' ..." << std::endl;
+		set.insert(string);
+		return false;
+	} else {
+		return true;
+	}
+}
+
 void find_relevant_string(Context & context)
 {
 	auto input = context.relevance["input"];
@@ -14,6 +41,10 @@ void find_relevant_string(Context & context)
 		&contexts[inputcontext + "-2"].relevance,
 		&contexts[inputcontext + "-3"].relevance
 	};
+	// the problem is not that we are returning many values, but that we are rereviewing the nearness of values, forever.
+	//decltype(Context::relevance)* old_ways = &contexts[inputcontext + "-old-ways"];
+		// TODO: prevent cycling by storing thing-nearness in old_ways, and not repeating.
+		// you could also make a context for each thing-context-nearness.
 
 	if (!ideas[0]->empty()) {
 		auto it = ideas[0]->begin();
@@ -23,10 +54,12 @@ void find_relevant_string(Context & context)
 		return;
 	}
 
+	// propose: 3 states: helps-find, good-result, and already-tried
+
 	if (contexts[inputcontext].relevance.empty()) {
 		contexts[inputcontext].relevance["seen-before"] = "yes";
-		ideas[1]->insert({input,input});
-		ideas[1]->insert({contextuality,contextuality});
+		ideas[0]->insert({input,input});
+		ideas[0]->insert({contextuality,contextuality});
 	}
 
 	size_t index = 1;
@@ -45,7 +78,9 @@ void find_relevant_string(Context & context)
 
 			// way #2: find context keys
 			for (auto pair : other) {
+				if (already_tried(inputcontext, pair.first)) { continue; }
 				ideas[0]->insert({pair.first,pair.first});
+				return find_relevant_string(context);
 			}
 
 			// way #3: find context values based on using contents as keys and other contents as contexts
@@ -53,21 +88,26 @@ void find_relevant_string(Context & context)
 				auto & otheridea = pair.second;
 				if (other.count(otheridea)) {
 					auto & information = other[otheridea];
+					if (already_tried(inputcontext, information)) { continue; }
 					ideas[0]->insert({information,information});
+					return find_relevant_string(context);
 				}
 			}
 		}
 		for (auto it = oldideas.begin(); it != oldideas.end();) {
-			typename std::string::size_type idx, lastidx = ~0;
+			typename std::string::size_type idx, lastidx = 0;
 			auto oldidea = it->second;
 			// way #1: break hyphens
-			if ((idx = oldidea.find("-")) != std::string::npos) {
-				do {
-					auto str = oldidea.substr(lastidx + 1, idx);
-					ideas[0]->insert({str, str});
-					lastidx = idx;
-				} while((idx = oldidea.find("-")) != std::string::npos);
-				break;
+			if (oldidea.find("-") != std::string::npos) {
+				for (auto & substring : string_split(oldidea, "-")) {
+					if (!already_tried(inputcontext, substring)) {
+						ideas[0]->insert({substring, substring});
+						// stopping at 1st idea found supports goal of checking with use
+						// on value of information.  [to know where to store it]
+						// see EXPANSION-ATTEMPT down in file
+						return find_relevant_string(context);
+					}
+				}
 			}
 
 
@@ -86,7 +126,59 @@ void find_relevant_string(Context & context)
 		// 	-> another approach is to build links in named contexts to parts of strings.
 		// 	-> e.g. maybe-relevance: [dehyphenation]:[maybe-it-will-work]
 	}
-
-	return find_relevant_string(context);
-
 }
+
+/*
+EXPANSION ATTEMPT (attempt to consolidate to clarity below it, also labeled with same label)
+
+make find-relevance stop and reconsider whenever it found a new relevant concept helped build access to its
+proper structure.
+        [karl is focusing on using above clearer expression to build more clear descriptions of a process for
+         working with stored state on finding relevance]
+
+                something about a hierarchy of how-helpful a concept might be
+                and combining things that are more-helpful together to see if any results are more-helpful
+                it looks like you want to place ideas at different spots on that hierarcht than always at the top.
+                        means feedback on helpfulness.
+
+                                feedback-on-helpfulness: how does it relate to place-on-hierarchy
+                                        if we can move topmost concepts down quickly (or never even place them at the top)
+                                        then we can expand inner-product with more relevance.
+                                                (for only-look-at-and-calculate-parts-of-inner-product-that-help)
+                        so, for investment it would be best if we could check if a concept is helpful or not before
+                        deciding where to place it.
+                        for current-situation, _any_ information where system can adjust value of a concept is helpful.
+                                presently just-used concepts are going to be either in level 0 or level 1.
+                                        if they are helpful, we want them in level 1 or 0
+                                        if they are not-helpful, we want them lower
+                                                we actually have n-depth to handle deep exploration.
+                                                [it looks like karl's brain has 3-depth, with depth=3 only used once depth=2
+                                                 is migrated to depth=1 from exhaustion of depth=0]^B
+                                                [karl's relevancy is depth=1 first.  depth=0 is active memory, and there are
+                                                 links between brainstorming and active memory.]
+                                                        [so it would be clearer for karl if the system didn't hold depth=0]
+                                                [above are very rough, to solve goal with limited resources]
+EXPANSION ATTEMPT
+
+make find-relevance stop and reconsider whenever it found a new relevant concept helped build access to its
+proper structure. {{after this thorough investigation, a lot of the introspective links feel altered, now}}
+
+Summary of Relevancy Process, more expansion towards goal:
+        [as relevancy, it sounds like we have a 3-depth list of ideas]
+        [and the user of us advises us on what to do with ideas in the upper level, or new ideas we find in lower levels]
+                [it sounds like we just offer our upper level to the user, read-only]
+                [and they can decide what to expand more]
+
+                        [how is third depth managed?]
+                                [third depth is ideas that didn't work, that haven't had their inner product fully expanded
+                                 with any new ideas.  since new ideas are new, that means all used-up ideas.]
+                                [ideas in third level are associated with ideas in second level that they might help in combination with]
+                                [ideas in second level are in the midst of having their inner product expanded with ideas in the
+                                 first level]
+                [inner product is made between concepts in the store, with relevant wayness, first level first]
+                        [so we also have relevancy on combination-habits] {this is in the design queue, not currently present, not valued focus atm}
+                [new concepts are found using relevant-first similarity habits, with creativity to come up with new]
+                        {for this we have things like similar-string, or usually-useful-after-the-other-is-useful}
+
+        [this process is the most important process we know for making intellect based on human cognition.]
+*/                                                                                                                                    
