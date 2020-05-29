40ef634181eafabd4ecdc9ccbb049e0f9b7ac842
user <example@example.com>
2020-05-29 18:34:43 +0000

    considering likelihood

--- simple-friend_tool/simple.cpp
+++ simple-friend_tool/simple.cpp
@@ -2,6 +2,7 @@
 #include "tools.hpp"
 
 using amount = double;
+using quantity = size_t;
 
 namespace next_word
 {
@@ -12,7 +13,10 @@ namespace next_word
 	class thought
 	{
 	public:
-		virtual string & word() = 0;
+		thought(string word)
+		: member-word(word) { }
+
+		virtual string & word() { return member-word; }
 
 		virtual bool next-is-known() = 0;
 		virtual ref next() = 0;
@@ -23,15 +27,17 @@ namespace next_word
 
 		shared_ptr<any> more; // could turn into a virtual function
 		//map<amount, ref> next;
+
+	protected:
+		string member-word;
 	};
 
 	class in-a-row : public thought
 	{
 	public:
 		in-a-row(string word)
-		: member-word(word)
+		: thought(word)
 		{ }
-		string & word() override { return member-word; }
 
 		bool next-is-known() override { return next().get() != nullptr; }
 		ref next() override { return member-next; }
@@ -40,7 +46,7 @@ namespace next_word
 		{
 			if (next-is-known() && this->next() != next) {
 				if (capacity) { return false; }
-				cerr << " -- I want a way to reach out to more here!" << endl;
+				cerr << " -- I want a way to reach out to more here!  I didn't expect to hear this!" << endl;
 				// might randomly just return true here
 			}
 			member-next = next;
@@ -49,8 +55,74 @@ namespace next_word
 
 
 	private:
-		string member-word;
 		ref member-next;
+	};
+
+	class likelihood : public thought
+	{
+	public: // we have unsureness with likelihood don't we
+		// don't fret about stats, try to do quick solution
+		likelihood(string word)
+		: thought(word), member-total-experiences(0)
+		{ }
+		
+		bool next-is-known() override
+		{
+			if (member-total-experiences > 2 && member-experiences.size() == 1) {
+				return true;
+			} else if (member-total-experiences > 0) {
+				amount confidence = get-best()->second / amount(member-total-experiences + 1) * 100;
+				if (confidence <= 50) {
+					cerr << " -- I want a way to reach out to more here.  I don't know how to convey that I have a poor guess." << endl;
+				} else {
+					cerr << " -- I want a way to reach out to more here.  I don't know how to convey that I expect " << tools::quote_special(next()->word()) << " with " << quantity(confidence) << "% confidence." << endl;
+					return true;
+				}
+			}
+			return false;
+		}
+
+		ref next() override
+		{
+			return get-best()->first;
+		}
+
+		bool note-next(reference next, bool capacity) override
+		{
+			if (member-experiences.size() == 1 && member-total-experiences > 1 && next != member-experiences.begin()->first) {
+				if (capacity) { return false; }
+				cerr << " -- I want a way to reach out to more here.  I used to know how to predict this and no longer do." << endl;
+			} else if (member-total-experiences < 3) {
+				cerr << " -- How helpful to consider: " << tools::quote_special(next->word()) << " =)" << endl;
+			} else {
+				cerr << " -- How boring to see again: " << tools::quote_special(next->word()) << endl;
+			}
+			if (member-experiences.count(next) == 0) {
+				member-experiences[[next]] = 1;
+			} else {
+				++ member-experiences[[next]];
+			}
+			++ member-total-experiences;
+			if (member-experiences.size() > 1) {
+				cerr << " -- I want a way to reach out to more here.  I don't know what to do with these multiple options." << endl;
+			}
+		}
+		
+	private:
+		map<ref, quantity> member-experiences;
+		quantity member-total-experiences;
+
+		decltype(member-experiences)::iterator get-best()
+		{
+			auto best = member-experiences.begin();
+			for (auto start = best; start != member-experiences.end(); ++ start)
+			{
+				if (start->second > best->second) {
+					best = start;
+				}
+			}
+			return best;
+		}
 	};
 
 	class book {
@@ -72,20 +144,20 @@ namespace next_word
 		static constexpr char const * end-of-conversation = "special-end-of-conversation";
 
 		thinker() {
-			first-thought.reset(new in-a-row(start-of-conversation));
+			first-thought.reset(new likelihood(start-of-conversation));
 			last-thought = first-thought;
 			current-thought = first-thought;
 		}
 	
 		void hear(string word, bool internal = false) {
 			if (!internal) {
-				cerr << " -- Heard: " << word << endl;
+				cerr << " -- Heard: " << tools::quote_special(word) << endl;
 				// if internal we could recurse making a sublist of words for thinking, referencing memory -karl-idea
 			}
 
 			auto & word-ref = current_book.index[[word]];
 			if (!word-ref) {
-				word-ref.reset(new in-a-row({word}));
+				word-ref.reset(new likelihood({word}));
 			}	
 			if (current-thought->next-is-known()) {
 				if (current-thought->next()->word() != word) {
@@ -94,7 +166,8 @@ namespace next_word
 					cerr << " -- Yes, '" << word << "' comes after '" << current-thought->word() << "'... " << endl;
 				}
 			} else {
-				cerr << " -- How new to " << (internal ? "think" : "hear") << ": '" << word << "' =)" << endl;
+				// TODO: we also do this inside likelihood::note-next, and it doesn't know if it's internal.  could fix linebreak by accumulating words as a plan, dunno [breaks model-feedback-with-listener].
+				cerr << " -- How new to " << (internal ? "think" : "hear") << ": " << tools::quote_special(word) << " =)" << endl;
 			}
 			last-thought = current-thought;
 			current-thought = word-ref;
@@ -104,7 +177,7 @@ namespace next_word
 			if (current-thought->next-is-known()) {
 				hear(current-thought->next()->word(), true);
 			} else {
-				cerr << " -- I need a way to reach out to more here!" << endl;
+				cerr << " -- I need a way to reach out to more here!  I don't know what to do!" << endl;
 				hear(silence, true);
 			}
 			return current-thought->word();
@@ -127,11 +200,7 @@ int main()
 		}
 		while (true) {
 			word = thinker.say();
-			if (word.compare(0, 8, "special-") == 0) {
-				cout << " -- [" << word << "]";
-			} else {
-				cout << " " << word;
-			}
+			cout << " " << tools::quote_special(word, false);
 			if (word == next_word::thinker::silence || word == next_word::thinker::end-of-conversation) { break; }
 		}
 		cout << endl << endl;
--- simple-friend_tool/tools.hpp
+++ simple-friend_tool/tools.hpp
@@ -6,6 +6,30 @@
 
 namespace tools {
 
+bool starts_with(std::string const & word, std::string const & start)
+{
+	return word.compare(0, start.size(), start) == 0;
+}
+
+char const * without_start(std::string const & word, std::string const & start)
+{
+	return word.c_str() + start.size();
+}
+
+std::string mutate_start_into_endfixes(std::string const & word, char const * word_prefix, char const * word_suffix, std::string const & start, std::string const & start_prefix, char const * start_suffix)
+{
+	if (starts_with(word, start)) {
+		return start_prefix + without_start(word, start) + start_suffix;
+	} else {
+		return word_prefix + word + word_suffix;
+	}
+}
+
+std::string quote_special(std::string const & word, bool quotes = true)
+{
+	return mutate_start_into_endfixes(word, quotes ? "'" : "", quotes ? "'" : "", "special-", "[", "]");
+}
+
 std::string line_word()
 {
 	// this part stores a line-expression, and provides the words
