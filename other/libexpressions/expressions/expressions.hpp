1034572e4dce5aaa9efa0359c31bae60ece2da34
Karl Semich <karl@Karl-Semich--0xloem-at-gmail-dot-com--txt-1-207-518-8256.Caring.goals.words.and.actions.are.more.important.than.anything>
2020-06-15 05:01:48 -0400

    added list of operators

--- other/libexpressions/expressions/expressions.hpp
+++ other/libexpressions/expressions/expressions.hpp
@@ -14,6 +14,142 @@
 #include <functional>
 #include <vector>
 
+enum operator_enum
+{
+	plus,
+	minus, negate,
+	multiplies,
+	divides,
+	modulus,
+	bit_xor,
+	bit_and,
+	bit_or,
+	bit_not,
+	logical_not,
+	assign,
+	less,
+	greater,
+	assign_plus,
+	assign_minus,
+	assign_multiplies,
+	assign_divides,
+	assign_modulus,
+	assign_bit_xor,
+	assign_bit_and,
+	assign_bit_or,
+	shift_left,
+	shift_right,
+	assign_shift_left,
+	assign_shift_right,
+	equal_to,
+	not_equal_to,
+	less_equal,
+	greater_equal,
+	//three_way,
+	logical_and,
+	logical_or,
+	preincrement,
+	predecrement,
+	postincrement,
+	postdecrement,
+	comma,
+	star,
+	arrow_star,
+	arrow,
+	index,
+	call,
+	cast, // not considered yet
+	literal // not considered yet
+};
+
+
+template <operator_enum, typename Return, typename Self, typename...Args>
+class op;
+#define __opN(name, symbol) \
+template <typename Return, typename Self, typename... Args> \
+class op<operator_enum::name, Return, Self, Args...> \
+{ \
+	Return operator symbol (Args & ... args) \
+	{ \
+		Self & self = *(Self*)this; \
+		return self.template syntax_operate<Return>(name, args...); \
+	} \
+};
+#define __op3(name, symbol) \
+template <typename Return, typename Self, typename Right> \
+class op<operator_enum::name, Return, Self, Right> \
+{ \
+	Return operator symbol (Right & right) \
+	{ \
+		Self & self = *(Self*)this; \
+		return self.template syntax_operate<Return>(name, right); \
+	} \
+};
+#define __op2(name, symbol) \
+template <typename Return, typename Self> \
+class op<operator_enum::name, Return, Self> \
+{ \
+	Return operator symbol () \
+	{ \
+		Self & self = *(Self*)this; \
+		return self.template syntax_operate<Return>(name); \
+	} \
+}; \
+
+__op3(plus, +)
+__op3(minus, -) __op2(negate, -)
+__op3(multiplies, *)
+__op3(divides, /)
+__op3(modulus, %)
+__op3(bit_xor, ^)
+__op3(bit_and, &)
+__op3(bit_or, |)
+__op2(bit_not, ~)
+__op2(logical_not, !)
+__op3(assign, =)
+__op3(less, <)
+__op3(greater, >)
+__op3(assign_plus, +=)
+__op3(assign_minus, -=)
+__op3(assign_multiplies, *=)
+__op3(assign_divides, /=)
+__op3(assign_modulus, %=)
+__op3(assign_bit_xor, ^=)
+__op3(assign_bit_and, &=)
+__op3(assign_bit_or, |=)
+__op3(shift_left, <<)
+__op3(shift_right, >>)
+__op3(assign_shift_left, <<=)
+__op3(assign_shift_right, >>=)
+__op3(equal_to, ==)
+__op3(not_equal_to, !=)
+__op3(less_equal, <=)
+__op3(greater_equal, >=)
+//__op(three_way, <=>) // this is simply for implementing 3 comparators at once
+__op3(logical_and, &&)
+__op3(logical_or, ||)
+__op2(preincrement, ++)
+__op2(predecrement, --)
+__op3(postincrement, ++)
+__op3(postdecrement, --)
+#define __COMMA ,
+__op3(comma, __COMMA)
+#undef __COMMA
+__op2(star, *)
+__op3(arrow_star, ->*)
+__op2(arrow, ->)
+__op3(index, [])
+__opN(call, ())
+#undef __op3
+#undef __op2
+#undef __opN
+
+	/*
+template <typename Return, typename Self>
+class op_arithmetic : public op<plus, 
+*/
+
+
 template <typename T>
 class operable
 {
@@ -28,6 +164,12 @@ public:
 	struct index_out_of_range : public std::exception { };
 	struct nonsimple_result : public std::exception { };
 	// to enumerate these without known values we would need a graph of interrelation
+	// 	to accumulate possibility, we'd need to make sets that can:
+	// 		-> query if soemthing is in it
+	// 		-> work with how to change that
+	// 		-> combine it with other sets
+	// 		-> create something that is within it
+	// 		these sets are defined by expressions,
 	struct bounded_result : public nonsimple_result
 	{
 		T bound;
@@ -138,6 +280,28 @@ namespace operators
 	__b3(greater) __b3(less)
 	__b3(greater_equal) __b3(less_equal)
 
+	template <typename T> 
+	std::function<T(T const &, bool const &)> func_same_if = [](T const &a, bool const & b) -> T {
+		if (b) { return a; }
+		else { throw operable<std::any>::bounded_result(a, false, true, true); }
+	};
+	template <typename T>
+	std::function<T(bool const &, T const &)> func_if_same = [](bool const & a, T const & b) -> T {
+		if (a) { return b; }
+		else { throw operable<std::any>::bounded_result(b, false, true, true); }
+	};
+	template <typename T> 
+	std::function<T(T const &, bool const &)> func_same_if_not = [](T const &a, bool const & b) -> T {
+		if (!b) { return a; }
+		else { throw operable<std::any>::bounded_result(a, false, true, true); }
+	};
+	template <typename T>
+	std::function<T(bool const &, T const &)> func_if_not_same = [](bool const & a, T const & b) -> T {
+		if (!a) { return b; }
+		else { throw operable<std::any>::bounded_result(b, false, true, true); }
+	};
+
+
 	// equal-to can't be solved for inequal, produces a range
 	// let's leave it out for now?
 	// 	no this is great.
@@ -146,13 +310,6 @@ namespace operators
 	// we would need a type that represents a range, which is quite
 	// fine.
 	// what about inequal?  this produces a set of anything-but.
-	// 	what might you want to do with such a set?
-	// 		-> query if soemthing is in it
-	// 		-> work with how to change that
-	// 		-> combine it with other sets
-	// 		-> create something that is within it
-	// 		these sets are defined by expressions,
-	// 		and nested use of them could get complicated
 
 	__f3(logical_and) __f3(logical_or) __f2(logical_not)
 
@@ -177,22 +334,10 @@ namespace operators
 	operable_any_3<T> divides_any("/", func_divides<T>, func_multiplies<T>, func_divides_reverse<T>);
 	// a = b == c
 	template <typename T>
-	operable_any_3<bool, T> equal_to_any("==", func_equal_to<T>, [](T const &c, bool const & a) -> T {
-		if (a) { return c; }
-		else { throw operable<std::any>::bounded_result(c, false, true, true); }
-	}, [](bool const & a, T const & b) -> T {
-		if (a) { return b; }
-		else { throw operable<std::any>::bounded_result(b, false, true, true); }
-	});
+	operable_any_3<bool, T> equal_to_any("==", func_equal_to<T>, func_same_if<T>, func_if_same<T>);
 	// a = b != c
 	template <typename T>
-	operable_any_3<bool, T> not_equal_to_any("!=", func_not_equal_to<T>, [](T const &c, bool const & a) -> T {
-		if (!a) { return c; }
-		else { throw operable<std::any>::bounded_result(c, false, true, true); }
-	}, [](bool const & a, T const & b) -> T {
-		if (!a) { return b; }
-		else { throw operable<std::any>::bounded_result(b, false, true, true); }
-	});
+	operable_any_3<bool, T> not_equal_to_any("!=", func_not_equal_to<T>, func_same_if_not<T>, func_if_not_same<T>);
 	// a = b > c
 	template <typename T>
 	operable_any_3<bool, T> greater_any(">", func_greater<T>, [](T const &c, bool const & a) -> T {
@@ -289,21 +434,26 @@ namespace operators
 	operable_any_2<T> bit_not_any("~", func_bit_not<T>, func_bit_not<T>);
 };
 
-// so, we might want to implement operators in such a way that we can quickly add functionality.
-// we could say make a base class or soemthing, that provides all the operators such that they use some operable-derived class or somesuch
-// 	we have a way to get operables that are kinda automatic.
-// 	so, somehow use derived instead
+// We can maybe add functionality to operators quickly by making
+// a class that has operators, and deriving other things from it.
+// It would need to use the operable class, I suppose? and call a handler?
+// likely some way to simplify.
+
 
 // for implementing more of this, we'll need a way to pass everything to a particular set of operator classes / handlers / whatever
 // some way to proxy or somesuch.  make a group of operators that wrap others.
 
+// exprssion should work okay with operable_any.
+#include <memory>
 template <typename T>
 class expression
 {
 public:
+	// takes as normal, but passes to operate function how?
 	expression<T> operator+(expression<T> const & other) {
-		//return T(
 	}
+	operator_enum op;
+	std::vector<std::shared_ptr<expression>> parts;
 };
 
 // optional simple cmp implementation for any
