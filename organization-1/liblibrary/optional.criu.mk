00f3914f67ccdd86577e032611bc5ab41108c3df
karl <karl@teres>
2020-07-10 06:15:37 -0400

    check criu functionality when enabling criu

--- organization-1/liblibrary/optional.criu.mk
+++ organization-1/liblibrary/optional.criu.mk
@@ -1,4 +1,4 @@
-HAVE_CRIU=$(shell pkg-config --exists criu && echo 1)
+HAVE_CRIU=$(shell pkg-config --exists criu && criu check && echo 1)
 ifeq ($(HAVE_CRIU),1)
 	LDLIBS+=$(shell pkg-config --libs criu)
 	CPPFLAGS+=$(shell pkg-config --cflags criu) -DLIBLIBRARY_HAVE_CRIU=1
