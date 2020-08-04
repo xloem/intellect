77fcf5769c8f3e9580a3a77261de2f73cf7bfbd8
Karl Semich <0xloem@gmail.com>
2020-08-04 16:48:54 -0400

    context compiles

---
+++ organization-1/node4/context.cpp
@@ -0,0 +1,8 @@
+#include "context.hpp"
+
+#include <library/string.hpp>
+using namespace library;
+
+DEFINE sugar::context::default-category;
+DEFINE sugar::context::kind-outer;
+DEFINE sugar::context::kind-category;
--- organization-1/node4/context.hpp
+++ organization-1/node4/context.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "reference.hpp"
+
 // thread-local context, categorized by other contexts [can be made infinitely recursive =) nesting ambiguities could arise if doing that]
 
 // [note: we are interested in making a step towards being agnostic to the node [database] backend used]
@@ -10,34 +12,43 @@ class context : public reference
 {
 public:
 	DECLARE default-category;
-	DECLARE outer;
-	DECLARE category;
+	DECLARE kind-outer;
+	DECLARE kind-category;
 
-	context(reference category = context::default-category(), reference identity = ()) // calling static function for now, fix later
-	: reference(identity), category(category), this-context(identity), last-context(contexts.kind-get(category))
+	context(reference category = context::default-category(), reference identity = {}) // calling static function for now, fix later
+	: reference(identity), category(category), last-context(contexts.kind-get(category))
 	{
 		if (reference::null() != last-context) {
-			this-context.kind-set(outer-context(), last-context);
+			kind-set(context::kind-outer(), last-context);
 		}
-		kind-set(context::category(), category);
-		contexts.kind-set(category, this-context);
+		kind-set(context::kind-category(), category);
+		contexts.kind-set(category, *this);
 	}
-	~context() nothrow(false)
+	~context() //no_throw(false)
 	{
-		reference replaced-context = contexts.kind-get(categry, last-context);
+		verify-agreements();
 		contexts.kind-set(category, last-context);
-		if (*this != this-context) { throw agreement-mistake(); }
-		if (replaced-context != this-context) { throw agreement-mistake(); }
-		if (replaced-context.kind-get(outer-context()) != last-context) { throw agreement-mistake(); }
 	}
 
 	static reference get(reference category = context::default-category())
+	{
+		reference result = contexts.kind-get(category);
+		(*(context*)(&result)).verify-agreements();
+		return result;
+	}
 
 private:
+	void verify-agreements()
+	{
+		reference this-context = contexts.kind-get(category);
+		if (*this != this-context) { throw agreement-mistake(); }
+		reference replaced-context = this-context.kind-get(context::kind-outer());
+		if (replaced-context != last-context) { throw agreement-mistake(); }
+	}
+
 	static thread_local reference contexts;
 
 	reference category;
-	reference this-context;
 
 	reference last-context;
 };
