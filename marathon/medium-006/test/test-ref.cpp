c8079a0f8f56e2d6175521f9eca80a0e8129a85a
Karl Semich <0xloem@gmail.com>
2020-08-08 10:52:32 -0400

    -

---
+++ marathon/medium-006/test/test-ref.cpp
@@ -0,0 +1,10 @@
+#include "../ref.hpp"
+
+#include <stdexcept>
+
+struct testref { testref()
+{
+	// todo: test basic function of a ref class
+	// making and retrieving links, probably not using operators
+}
+} reftester;
--- marathon/small-007/ref-001.hpp
+++ marathon/small-007/ref-001.hpp
@@ -16,3 +16,10 @@ struct concept;
 // refers to concept data, can be copied lightweight
 class ref {
 };
+
+// holds concept data; rarely used directly
+// we were thinking link objects would be eased if iterators uniquely differentiated
+// links better
+//
+// note: std::multimap does not invalidate iterators on mutation
+// std::unordered_multimap does
